4be5e96e43f9d521b6d03537f12a892fd49df80b
admin <admin>
2018-12-23 18:39:44 +0800

    some change

--- app/src/main/java/com/bt/andy/fusheng/adapter/PutProDetailAdapter.java
+++ app/src/main/java/com/bt/andy/fusheng/adapter/PutProDetailAdapter.java
@@ -1,10 +1,14 @@
 package com.bt.andy.fusheng.adapter;
 
 import android.content.Context;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.bt.andy.fusheng.R;
@@ -53,7 +57,7 @@ public class PutProDetailAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(final int i, View view, ViewGroup viewGroup) {
         final MyViewHolder viewHolder;
         if (null == view) {
             view = View.inflate(mContext, R.layout.adapter_item_putdetail, null);
@@ -76,13 +80,14 @@ public class PutProDetailAdapter extends BaseAdapter {
             @Override
             public void onClick(View view) {
                 //打开选择仓库
-                openSelectStore(viewHolder.tv_store);
+                openSelectStore(i, viewHolder.tv_store, 0);
             }
         });
         viewHolder.tv_position.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                //打开选择仓库
+                openSelectStore(i, viewHolder.tv_position, 1);
             }
         });
         viewHolder.tv_sjsum.setOnClickListener(new View.OnClickListener() {
@@ -102,8 +107,32 @@ public class PutProDetailAdapter extends BaseAdapter {
 
     private PopupOpenHelper openHelper;
 
-    private void openSelectStore(View view) {
-        openHelper = new PopupOpenHelper(mContext, view, R.layout.popup_select_store);
-
+    private void openSelectStore(final int position, final TextView contview, final int kind) {
+        openHelper = new PopupOpenHelper(mContext, contview, R.layout.popup_select_store);
+        openHelper.openPopupWindow(true, Gravity.CENTER);
+        openHelper.setOnPopupViewClick(new PopupOpenHelper.ViewClickListener() {
+            @Override
+            public void onViewClickListener(PopupWindow popupWindow, View inflateView) {
+                ListView lv_store = inflateView.findViewById(R.id.lv_store);
+                LvNameAndIDAdapter nameAndIDAdapter;
+                if (0 == kind) {
+                    nameAndIDAdapter = new LvNameAndIDAdapter(mContext, mStoreList, kind);//0仓库，1仓位
+                } else {
+                    nameAndIDAdapter = new LvNameAndIDAdapter(mContext, mPosList, kind);//0仓库，1仓位
+                }
+                lv_store.setAdapter(nameAndIDAdapter);
+                lv_store.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        if (0 == kind) {
+                            contview.setText(mStoreList.get(i).getHousename());
+                            //                            mList.get(position).setc
+                        } else {
+                            contview.setText(mPosList.get(i).getFname());
+                        }
+                    }
+                });
+            }
+        });
     }
 }
